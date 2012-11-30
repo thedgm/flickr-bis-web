7ace3b46efe4d7289a82445f4fa1251dafd5e315
DGM <thedgm@gmail.com>
2012-11-30 15:52:59 +0200

    started with html5 boilerplate as always

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+	header("Server: JunkBox");
+	header("X-Powered-By: Electricity");
+	header("HTTP/1.0 404 Not Found");
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '          ';
+	echo '</pre>';
+?>
